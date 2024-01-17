092ea22bda1b870a8fe1811f439fbd8c88de3ce2
rabby4 <mdrabbybd4747@gmail.com>
2024-01-18 02:02:59 +0600

    fix the background image problem

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,11 @@
-import SharedBanner from "@/components/banner/SharedBanner";
-import aboutImage from './public/about-image.jpg'
+'use client'
 
 const About = () => {
     return (
         <div>
-            <SharedBanner bannerImg={aboutImage} heading={'About'}></SharedBanner>
+            <div className="text-center py-24 h-11" style={{backgroundImage: "url('https://i.ibb.co/vQxmhLd/about-image.jpg')"}}  >
+                <h2 className='text-3xl font-bold'>About</h2>
+            </div>
         </div>
     );
 };
--- src/components/banner/SharedBanner.js
+++ src/components/banner/SharedBanner.js
@@ -3,8 +3,8 @@ import React from 'react';
 const SharedBanner = ({bannerImg, heading}) => {
     return (
         <div>
-            <div className='text-center py-24' style={{backgroundImage: `url(./public/about-image.jpg)`}}>
-                <h2 className='text-4xl font-bold'>{heading}</h2>
+            <div className='text-center py-24 h-11' style={{backgroundImage: `url(${bannerImg})`}}>
+                <h2 className='text-3xl font-bold'>{heading}</h2>
             </div>
         </div>
     );
