d6b769564ede56b3c31790e3a18ebeb32858f74d
nazmulhossen1 <nazmulhossen.main@gmail.com>
2024-01-18 00:38:00 +0600

    Solved parsing error and make basic navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/Navbar/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
       <body className={inter.className}>
-        
+        <Navbar></Navbar>
       {children}
       </body>
     </html>
