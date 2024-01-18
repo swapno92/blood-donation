587782678144c4d8706d4f072b427fd6a4f5a77b
rabby4 <mdrabbybd4747@gmail.com>
2024-01-18 13:42:47 +0600

    create a about section on about page

--- src/components/banner/SharedBanner.js
+++ src/components/banner/SharedBanner.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const SharedBanner = ({bannerImg, heading}) => {
     return (
-        <div>
-            <div className='text-center py-24 h-11' style={{backgroundImage: `url(${bannerImg})`}}>
-                <h2 className='text-3xl font-bold'>{heading}</h2>
+        <div className=''>
+            <div className='hero text-center' style={{backgroundImage: `url(${bannerImg})`}}>
+            <div className="hero-overlay bg-black bg-opacity-40"></div>
+                <h2 className='text-5xl font-bold py-40 text-white'>{heading}</h2>
             </div>
         </div>
     );
