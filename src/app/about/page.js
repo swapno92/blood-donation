3d4e501acff4ea1c7b76edd2b9b6efce1f85e3f7
rabby4 <mdrabbybd4747@gmail.com>
2024-01-18 14:58:16 +0600

    install react icons and create service section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,13 @@
 import AboutSection from "@/components/about/AboutSection";
 import SharedBanner from "@/components/banner/SharedBanner";
+import ServiceSection from "@/components/services/ServiceSection";
 
 const About = () => {
     return (
         <>
             <SharedBanner bannerImg={'https://i.ibb.co/vQxmhLd/about-image.jpg'} heading={'About Us'} />
             <AboutSection />
+            <ServiceSection></ServiceSection>
         </>
     );
 };
