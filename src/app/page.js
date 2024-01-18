88fed1bf505244f22cd1a8fcf0ee501d8cb739a7
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2024-01-19 03:05:36 +0600

    [section] Added conter section and service section

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Banner from "@/components/HomeBanner/Banner";
 import Misstion from "@/components/HomeBanner/Misstion";
 import Donate from "@/components/HomeBanner/Donate";
+import ServiceSection from "@/components/services/ServiceSection";
+import CounterSection from "@/components/about/CounterSection";
 
 
 
@@ -8,8 +10,10 @@ const Home = () => {
   return (
     <div>
     <Banner></Banner>
-    <Donate></Donate>
-    <Misstion></Misstion>
+      <Donate></Donate>
+      <ServiceSection></ServiceSection>
+      <Misstion></Misstion>
+      <CounterSection></CounterSection>
     </div>
   );
 };
