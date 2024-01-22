335d7291ea8ee5dca6255c6a2e798232bab43369
nazmulhossen1 <nazmulhossen.main@gmail.com>
2024-01-22 20:12:37 +0600

    added like , comment button added

--- src/components/Community/Posts.js
+++ src/components/Community/Posts.js
@@ -1,10 +1,25 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import img1 from "../../../public/Banner img/img1.jpg";
 import img2 from "../../../public/Banner img/img2.jpg";
 import userimg from "../../../public/images/profile-circle-icon.png";
+import { AiFillLike, AiFillDislike } from "react-icons/ai";
+import { FaRegComment } from "react-icons/fa";
 import Image from "next/image";
 
 const Posts = () => {
+  const [likes, setLikes] = useState(16);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    if (isLiked) {
+      setLikes(likes - 1);
+    } else {
+      setLikes(likes + 1);
+    }
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div>
       <div className=" mx-auto bg-white rounded-xl overflow-hidden px-4 ">
@@ -35,11 +50,20 @@ const Posts = () => {
               src={img1}
               alt="Card"
             />
-            <div className="flex mt-4">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-full mr-2">
-                Like
+            <div className="flex items-center gap-4 mt-4 ">
+              <button
+                onClick={handleLike}
+                className="flex px-4 py-2 bg-primary text-white rounded-full cursor-pointer"
+              >
+                {isLiked ? (
+                  <AiFillDislike className="text-white text-xl text-center" />
+                ) : (
+                  <AiFillLike className="text-white text-xl text-center" />
+                )}
+                <span className="text-white ml-2">{likes}</span>
               </button>
-              <button className="bg-gray-500 text-white px-4 py-2 rounded-full">
+              <button className="bg-gray-500 text-white px-4 py-2 rounded-full flex items-center  gap-2">
+                <FaRegComment className="text-xl" />
                 Comment
               </button>
             </div>
@@ -76,11 +100,20 @@ const Posts = () => {
               src={img2}
               alt="Card"
             />
-            <div className="flex mt-4">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-full mr-2">
-                Like
+            <div className="flex items-center gap-4 mt-4 ">
+              <button
+                onClick={handleLike}
+                className="flex px-4 py-2 bg-primary text-white rounded-full cursor-pointer"
+              >
+                {isLiked ? (
+                  <AiFillDislike className="text-white text-xl text-center" />
+                ) : (
+                  <AiFillLike className="text-white text-xl text-center" />
+                )}
+                <span className="text-white ml-2">{likes}</span>
               </button>
-              <button className="bg-gray-500 text-white px-4 py-2 rounded-full">
+              <button className="bg-gray-500 text-white px-4 py-2 rounded-full flex items-center  gap-2">
+                <FaRegComment className="text-xl" />
                 Comment
               </button>
             </div>
@@ -98,7 +131,7 @@ const Posts = () => {
                 alt="User"
               />
               <div className="uppercase tracking-wide  text-gray-700 font-semibold">
-              J P Choyon Khan
+                J P Choyon Khan
               </div>
             </div>
 
@@ -117,11 +150,20 @@ const Posts = () => {
               src={img2}
               alt="Card"
             />
-            <div className="flex mt-4">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-full mr-2">
-                Like
+            <div className="flex items-center gap-4 mt-4 ">
+              <button
+                onClick={handleLike}
+                className="flex px-4 py-2 bg-primary text-white rounded-full cursor-pointer"
+              >
+                {isLiked ? (
+                  <AiFillDislike className="text-white text-xl text-center" />
+                ) : (
+                  <AiFillLike className="text-white text-xl text-center" />
+                )}
+                <span className="text-white ml-2">{likes}</span>
               </button>
-              <button className="bg-gray-500 text-white px-4 py-2 rounded-full">
+              <button className="bg-gray-500 text-white px-4 py-2 rounded-full flex items-center  gap-2">
+                <FaRegComment className="text-xl" />
                 Comment
               </button>
             </div>
@@ -139,7 +181,7 @@ const Posts = () => {
                 alt="User"
               />
               <div className="uppercase tracking-wide  text-gray-700 font-semibold">
-              MD Nazmul Huda
+                MD Nazmul Huda
               </div>
             </div>
 
@@ -158,11 +200,20 @@ const Posts = () => {
               src={img2}
               alt="Card"
             />
-            <div className="flex mt-4">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-full mr-2">
-                Like
+            <div className="flex items-center gap-4 mt-4 ">
+              <button
+                onClick={handleLike}
+                className="flex px-4 py-2 bg-primary text-white rounded-full cursor-pointer"
+              >
+                {isLiked ? (
+                  <AiFillDislike className="text-white text-xl text-center" />
+                ) : (
+                  <AiFillLike className="text-white text-xl text-center" />
+                )}
+                <span className="text-white ml-2">{likes}</span>
               </button>
-              <button className="bg-gray-500 text-white px-4 py-2 rounded-full">
+              <button className="bg-gray-500 text-white px-4 py-2 rounded-full flex items-center  gap-2">
+                <FaRegComment className="text-xl" />
                 Comment
               </button>
             </div>
@@ -180,7 +231,7 @@ const Posts = () => {
                 alt="User"
               />
               <div className="uppercase tracking-wide  text-gray-700 font-semibold">
-              Golam Rabby
+                Golam Rabby
               </div>
             </div>
 
@@ -199,11 +250,20 @@ const Posts = () => {
               src={img2}
               alt="Card"
             />
-            <div className="flex mt-4">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-full mr-2">
-                Like
+            <div className="flex items-center gap-4 mt-4 ">
+              <button
+                onClick={handleLike}
+                className="flex px-4 py-2 bg-primary text-white rounded-full cursor-pointer"
+              >
+                {isLiked ? (
+                  <AiFillDislike className="text-white text-xl text-center" />
+                ) : (
+                  <AiFillLike className="text-white text-xl text-center" />
+                )}
+                <span className="text-white ml-2">{likes}</span>
               </button>
-              <button className="bg-gray-500 text-white px-4 py-2 rounded-full">
+              <button className="bg-gray-500 text-white px-4 py-2 rounded-full flex items-center  gap-2">
+                <FaRegComment className="text-xl" />
                 Comment
               </button>
             </div>
