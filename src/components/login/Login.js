fcff51e70a7339b09809fe4f32882fef3b3fcad6
rabby4 <mdrabbybd4747@gmail.com>
2024-01-22 23:35:57 +0600

    customize registration page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,16 +1,19 @@
 'use client'
 import Lottie from 'lottie-react';
-import Heart from './../../../public/animations/heart.json'
+import Heart from './../../../public/animations/donate-blood.json'
 import { useForm } from "react-hook-form"
 import Link from 'next/link';
 import { IoArrowUndoOutline } from "react-icons/io5";
-import { FaFacebook } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaFacebook } from "react-icons/fa";
 import { SiGmail  } from "react-icons/si";
+import { useState } from 'react';
 
 
 
 
 const Login = () => {
+    const [showPassword, setShowPassword] = useState(false)
+
     const {
         register,
         handleSubmit,
@@ -38,25 +41,24 @@ const Login = () => {
                             <div className=' pb-0'>
                                 <p className='italic text-sm'>Save A Life</p>
                                 <h2 className='text-4xl font-semibold my-3'>SignIn to <span className='text-primary'>DropSync</span></h2>
-                                <p>Not A Member? <Link href={'/registration'} className='text-blue-500 hover:text-primary font-semibold'>Sign Up Now</Link></p>
+                                <p className='text-sm'>Not A Member? <Link href={'/registration'} className='text-blue-500 hover:text-primary font-semibold'>Sign Up Now</Link></p>
                                 <div className='divider'></div>
                             </div>
                            
                         <form onSubmit={handleSubmit(onSubmit)} className="card-body p-0 my-5">
                             
                             <div className="form-control">
-                                {/* <label className="label py-0">
-                                    <span className="text-lg font-semibold">Email</span>
-                                </label> */}
                                 <input {...register("email", { required: true })} type="email" placeholder="Your Email..." className="input focus:border-0 focus-within:outline-none border-0 border-b-2 border-gray-400 focus:border-b-2 focus:border-primary rounded-none px-1" />
                                 {errors.email && <span className='text-red-600 text-sm mt-2'>Enter your Email address*</span>}
                             </div>
 
-                            <div className="form-control mt-4">
-                                {/* <label className="label py-0">
-                                    <span className="text-lg font-semibold">Password</span>
-                                </label> */}
-                                <input {...register("password", { required: true })} type="password" placeholder="Password" className="input focus:border-0 focus-within:outline-none border-0 border-b-2 border-gray-400 focus:border-b-2 focus:border-primary rounded-none px-1" />
+                            <div className="form-control mt-4 relative">
+                                <input {...register("password", { required: true })} type={showPassword ? 'text' : 'password'} placeholder="Password" className="input focus:border-0 focus-within:outline-none border-0 border-b-2 border-gray-400 focus:border-b-2 focus:border-primary rounded-none px-1" />
+                                <span className="cursor-pointer absolute right-3 top-4" onClick={() => setShowPassword(!showPassword)}>
+                                    {
+                                        showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                                    }
+                                </span>
                                 {errors.password && <span className='text-red-600 text-sm mt-2'>Enter your Password*</span>}
 
                                 <label className="label mt-4">
@@ -65,19 +67,23 @@ const Login = () => {
                             </div>
 
                             <div className="form-control mt-6">
-                                <button className="btn bg-primary border-0 hover:bg-secondary text-white text-lg shadow-lg shadow-primary/30">SignIn</button>
+                                <button className="btn bg-primary border-0 hover:bg-secondary text-white text-lg shadow-lg shadow-primary/30 rounded-sm">Sign In</button>
                             </div>
                         </form>
                         <div className="divider divider-error">OR</div>
                         <div className='flex items-center gap-5 mt-4'>
-                            <div className='w-1/2 flex items-center text-white bg-[#3b5998] justify-center p-2 gap-3'>
-                                <FaFacebook />
-                                <p>Facebook</p>
-                            </div>
-                            <div className='w-1/2 flex items-center text-white bg-[#dd4b39] justify-center p-2 gap-3'>
-                                <SiGmail   />
-                                <p>Google</p>
-                            </div>
+                            <button className='w-1/2'>
+                                <div className='flex items-center text-white bg-[#3b5998] justify-center p-2 gap-3'>
+                                    <FaFacebook />
+                                    <p>Facebook</p>
+                                </div>
+                            </button>
+                            <button className='w-1/2'>
+                                <div className='flex items-center text-white bg-[#dd4b39] justify-center p-2 gap-3'>
+                                    <SiGmail   />
+                                    <p>Google</p>
+                                </div>  
+                            </button>
                         </div>
                     </div>
                 </div>
