557bf46ce4d0e86493a3295cb776cc7cc1a8c859
rabby4 <mdrabbybd4747@gmail.com>
2024-01-24 00:12:52 +0600

    create new banner/slider in home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,15 @@ import Donate from "@/components/HomeBanner/Donate";
 import CounterSection from "@/components/about/CounterSection";
 import ServiceSection from "@/components/services/ServiceSection";
 import Newsletter from "@/components/Footer/Newsletter";
+import Banner2 from "@/components/HomeBanner/Banner2";
 
 
 
 const Home = () => {
   return (
     <div>
-    <Banner></Banner>
+    {/* <Banner></Banner> */}
+    <Banner2></Banner2>
     <Donate></Donate>
     <ServiceSection/>
     <Misstion></Misstion>
