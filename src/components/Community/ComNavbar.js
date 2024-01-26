1f0b7dc20e113f456cbda1f8b849097abef59bcf
rabby4 <mdrabbybd4747@gmail.com>
2024-01-26 19:35:28 +0600

    added navigration active style

--- src/components/Community/ComNavbar.js
+++ src/components/Community/ComNavbar.js
@@ -1,27 +1,34 @@
-import React from "react";
+'use client'
 import { MdPostAdd } from "react-icons/md";
 import { BiDonateBlood } from "react-icons/bi";
 import { RiDashboardLine } from "react-icons/ri";
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
 
 const ComNavbar = () => {
+  const pathname = usePathname()
+
   return (
     <section className="">
       <div className=" border-b-2   ">
         <div className="max-w-screen-md mx-auto ">
           <ul className="flex justify-center  md:items-center items-center  md:gap-5 gap-2 text-gray-500">
-            <li className="hover:bg-primary md:py-2 md: px-2 hover:text-white duration-500 rounded-sm" >
-              <Link href='/community/posts' className=" flex items-center gap-2 border-r-2 md:pr-4  text-[11px] pr-3 ">
+
+            <li className="" >
+              <Link href='/community/posts' className={`link px-2 py-2 no-underline font-semibold flex items-center gap-2 md:pr-4 text-[11px] pr-3 rounded-sm ${pathname === '/community/posts' ? 'text-white bg-primary' : 'font-semibold no-underline hover:bg-primary hover:text-white duration-500'} `}>
+
               <MdPostAdd className="md:text-2xl text-[15px]" /> All Posts
+              
               </Link>
             </li>
-            <li className="hover:bg-primary py-2 px-2 hover:text-white duration-500 rounded-sm" >
-              <Link href='/community/ourmission' className=" flex items-center gap-2 border-r-2 md:pr-4  text-[11px] pr-3 ">
+
+            <li className="" >
+              <Link href='/community/ourmission' className={`link px-2 py-2 no-underline font-semibold flex items-center gap-2 md:pr-4 text-[11px] pr-3 rounded-sm ${pathname === '/community/ourmission' ? 'text-white bg-primary' : 'font-semibold no-underline hover:bg-primary hover:text-white duration-500'} `}>
               <BiDonateBlood className="md:text-2xl text-[15px]" /> Campaign
               </Link>
             </li>
-            <li className="hover:bg-primary py-2 px-2 hover:text-white duration-500 rounded-sm" >
-              <Link href='/community/dasboardlayout' className="flex items-center gap-2   text-[11px]">
+            <li className="" >
+              <Link href='/community/dasboardlayout' className={`link px-2 py-2 no-underline font-semibold flex items-center gap-2 md:pr-4 text-[11px] pr-3 rounded-sm ${pathname === '/community/dasboardlayout' ? 'text-white bg-primary' : 'font-semibold no-underline hover:bg-primary hover:text-white duration-500'} `}>
               <RiDashboardLine className="md:text-2xl text-[15px]" /> Dashboard
               </Link>
             </li>
@@ -35,3 +42,4 @@ const ComNavbar = () => {
 };
 
 export default ComNavbar;
+// className="flex items-center gap-2 md:pr-4 text-[11px] pr-3 "
