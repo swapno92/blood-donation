b2ddcacde265473205c8a82bba5671f81d578d65
tuhinhossain123 <csetuhin55@gmail.com>
2024-02-12 20:21:43 +0600

    our campaing page designd

--- src/components/Community/Campaings/Campaings.js
+++ src/components/Community/Campaings/Campaings.js
@@ -1,109 +1,92 @@
 import { BsCalendar2Check } from "react-icons/bs";
 import { FiClock } from "react-icons/fi";
 import { FaLocationDot } from "react-icons/fa6";
+import { FaArrowRightLong } from "react-icons/fa6";
+import campaingImg from "../../../../public/images/process_3.webp";
 import Image from "next/image";
-import campaingImg from "../../../../public/images/process_1.webp"
 const Campaings = () => {
   return (
     <div className="my-16">
       <div className="mb-10">
-        <h2 className="text-4xl text-center font-semibold">
-          DONATION CAMPAIGNS
+        <h2 className="text-4xl text-center font-semibold mb-4">
+          OUR CAMPAIGNS
         </h2>
-        {/* <hr className="w-[8rem] text-7xl mt-2 mx-auto"></hr> */}
-        <div className="divider w-[10rem] mx-auto divider-error"></div>
         <p className="text-center text-xl text-gray-500">
           Campaigns to encourage new donors to join and existing to continue to
           give blood.
         </p>
       </div>
-      {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-[80%] mx-auto">
-        <div className="card md:card-side w-full h-[90%] bg-gray-100 shadow-md rounded-none">
-          <div className="w-[50%] h-full">
-            <img
-              src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-              alt="Movie"
-              className="w-full h-full hidden md:block"
-            />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:w-[60%] mx-auto gap-8 px-6">
+        <div className="bg-gray-50 shadow-md rounded-b-none">
+          <div>
+          <Image  src={campaingImg} alt='campaing img' className="w-full"/>
           </div>
-          <div className="py-6 px-6 pb-4 space-y-4 w-[80%]">
-            <h2 className="flex items-center gap-2"><BsCalendar2Check  className="text-primary text-2xl"/>20 SEP, 2017</h2>
-            <h2 className="text-2xl font-semibold text-primary">WORLD BLOOD DONORS DAY</h2>
-            <p className="text-lg text-gray-500 text-medium">
-              Every year, on 14 June, countries around the world celebrate World
-              Blood Donor Day. The event serves to thank voluntary.
-              Every year, on 14 June, countries around the world celebrate
-            </p>
-            <div className="flex justify-between items-center">
-                <h2 className="flex items-center gap-1 text-gray-500"><FiClock className="text-primary text-xl"/>10:00am - 6:30pm</h2>
-                <h2 className="flex items-center gap-1 text-gray-500"><FaLocationDot className="text-primary text-xl"/>Dhaka, Bangladesh</h2>
+          <div className="py-2  px-5">
+            <div className="flex items-center gap-3 lg:pt-2 lg:pb-4">
+              <h2 className="flex items-center gap-3 text-sm">
+                <BsCalendar2Check className="text-primary text-2xl" />
+                20 SEP, 2017
+              </h2>
+              <div className="divider lg:divider-horizontal"></div>
+              <h2 className="flex items-center gap-2 text-gray-500">
+                <FiClock className="text-primary text-xl" />
+                10:00am - 6:30pm
+              </h2>
             </div>
-          </div>
-        </div>
-        <div className="card card-side w-full h-[90%] bg-gray-100 shadow-md rounded-none">
-          <div className="w-[50%] h-full">
-            <img
-              src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-              alt="Movie"
-              className="w-full h-full"
-            />
-          </div>
-          <div className="py-6 px-6 pb-4 space-y-4 w-[80%]">
-            <h2 className="flex items-center gap-2"><BsCalendar2Check  className="text-primary text-2xl"/>20 SEP, 2017</h2>
-            <h2 className="text-2xl font-semibold text-primary">WORLD BLOOD DONORS DAY</h2>
-            <p className="text-lg text-gray-500 text-medium">
-              Every year, on 14 June, countries around the world celebrate World
-              Blood Donor Day. The event serves to thank voluntary.
-              Every year, on 14 June, countries around the world celebrate
-            </p>
-            <div className="flex justify-between items-center">
-                <h2 className="flex items-center gap-1 text-gray-500"><FiClock className="text-primary text-xl"/>10:00am - 6:30pm</h2>
-                <h2 className="flex items-center gap-1 text-gray-500"><FaLocationDot className="text-primary text-xl"/>Dhaka, Bangladesh</h2>
-            </div>
-          </div>
-        </div>
-      </div> */}
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <a href="#">
-          <Image
-            className="rounded-t-lg"
-            src={campaingImg}
-            alt="frist camping"
-            width={400}
-            height={300}
-          />
-        </a>
-        <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-              Noteworthy technology acquisitions 2021
+            <h5 className="text-2xl lg:text-3xl font-medium text-primary mb-2 lg:mb-4">
+              WORLD BLOOD DONORS DAY
             </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
-          >
-            Read more
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
+
+            <p className="text-lg font-medium text-gray-500 pb-3">
+              Every year, on 14 June, countries around the world celebrate World
+              Blood Donor Day. The event serves to thank voluntary.
+            </p>
+            <h2 className="flex items-center gap-1 pb-6 text-gray-500">
+              <FaLocationDot className="text-primary text-xl" />
+              Dhaka, Bangladesh
+            </h2>
+            <div className="flex justify-end">
+              <button className="btn bg-primary text-white text-xl">
+                Read More <FaArrowRightLong />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-50 shadow-md rounded-b-none">
+          <div>
+          <Image  src={campaingImg} alt='campaing img' className="w-full"/>
+          </div>
+          <div className="py-2  px-5">
+            <div className="flex items-center gap-3 lg:pt-2 lg:pb-4">
+              <h2 className="flex items-center gap-3 text-sm">
+                <BsCalendar2Check className="text-primary text-2xl" />
+                20 SEP, 2017
+              </h2>
+              <div className="divider lg:divider-horizontal"></div>
+              <h2 className="flex items-center gap-2 text-gray-500">
+                <FiClock className="text-primary text-xl" />
+                10:00am - 6:30pm
+              </h2>
+            </div>
+            <h5 className="text-2xl lg:text-3xl font-medium text-primary mb-2 lg:mb-4">
+              WORLD BLOOD DONORS DAY
+            </h5>
+
+            <p className="text-lg font-medium text-gray-500 pb-3">
+              Every year, on 14 June, countries around the world celebrate World
+              Blood Donor Day. The event serves to thank voluntary.
+            </p>
+            <h2 className="flex items-center gap-1 pb-6 text-gray-500">
+              <FaLocationDot className="text-primary text-xl" />
+              Dhaka, Bangladesh
+            </h2>
+            <div className="flex justify-end">
+              <button className="btn bg-primary text-white text-xl">
+                Read More <FaArrowRightLong />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
