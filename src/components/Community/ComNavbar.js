1bcd79fa47730026ee645133c15dc4ed8dac0cd9
swapno92 <swapnoprograming92@gmail.com>
2024-02-20 13:43:36 +0600

    responsive done

--- src/components/Community/ComNavbar.js
+++ src/components/Community/ComNavbar.js
@@ -9,34 +9,51 @@ const ComNavbar = () => {
   const pathname = usePathname()
 
   return (
-    <section className="">
-      <div className=" border-b-2   ">
+    <section className="lg:top-[86px] md:top-[60px] top-[46px] z-10   sticky bg-white pb-3">
+      <div className=" border-b-2">
         <div className="max-w-screen-md mx-auto ">
           <ul className="flex justify-center  md:items-center items-center  md:gap-5 gap-2 text-gray-500">
-
-            <li className="" >
-              <Link href='/community/posts' className={`link px-2 py-2 no-underline font-semibold flex items-center gap-2 md:pr-4 text-[11px] pr-3 rounded-sm ${pathname === '/community/posts' ? 'text-white bg-primary' : 'font-semibold no-underline hover:bg-primary hover:text-white duration-500'} `}>
-
-              <MdPostAdd className="md:text-2xl text-[15px]" /> All Posts
-              
+            <li className="">
+              <Link
+                href="/community/posts"
+                className={`link px-2 py-2 no-underline font-semibold flex items-center gap-2 md:pr-4 text-[11px] pr-3 rounded-sm ${
+                  pathname === "/community/posts"
+                    ? "text-white bg-primary"
+                    : "font-semibold no-underline hover:bg-primary hover:text-white duration-500"
+                } `}
+              >
+                <MdPostAdd className="md:text-2xl text-[15px]" /> All Posts
               </Link>
             </li>
 
-            <li className="" >
-              <Link href='/community/campaings' className={`link px-2 py-2 no-underline font-semibold flex items-center gap-2 md:pr-4 text-[11px] pr-3 rounded-sm ${pathname === '/community/ourmission' ? 'text-white bg-primary' : 'font-semibold no-underline hover:bg-primary hover:text-white duration-500'} `}>
-              <BiDonateBlood className="md:text-2xl text-[15px]" /> Campaign
+            <li className="">
+              <Link
+                href="/community/campaings"
+                className={`link px-2 py-2 no-underline font-semibold flex items-center gap-2 md:pr-4 text-[11px] pr-3 rounded-sm ${
+                  pathname === "/community/ourmission"
+                    ? "text-white bg-primary"
+                    : "font-semibold no-underline hover:bg-primary hover:text-white duration-500"
+                } `}
+              >
+                <BiDonateBlood className="md:text-2xl text-[15px]" /> Campaign
               </Link>
             </li>
-            <li className="" >
-              <Link href='/community/dasboardlayout/profile' className={`link px-2 py-2 no-underline font-semibold flex items-center gap-2 md:pr-4 text-[11px] pr-3 rounded-sm ${pathname === '/community/dasboardlayout' ? 'text-white bg-primary' : 'font-semibold no-underline hover:bg-primary hover:text-white duration-500'} `}>
-              <RiDashboardLine className="md:text-2xl text-[15px]" /> Dashboard
+            <li className="">
+              <Link
+                href="/community/dasboardlayout/profile"
+                className={`link px-2 py-2 no-underline font-semibold flex items-center gap-2 md:pr-4 text-[11px] pr-3 rounded-sm ${
+                  pathname === "/community/dasboardlayout"
+                    ? "text-white bg-primary"
+                    : "font-semibold no-underline hover:bg-primary hover:text-white duration-500"
+                } `}
+              >
+                <RiDashboardLine className="md:text-2xl text-[15px]" />{" "}
+                Dashboard
               </Link>
             </li>
-            
           </ul>
         </div>
       </div>
-      
     </section>
   );
 };
