cf2f66854b17a5dec9d86d25f53a3d02b3585c92
JP Choyon Khan <kotjpckhan@gmail.com>
2024-03-06 16:25:48 +0600

    home page bug fix and some section are hidden

--- src/components/HomeBanner/Banner2.js
+++ src/components/HomeBanner/Banner2.js
@@ -8,9 +8,16 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Pagination } from "swiper/modules";
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import DonatRequest from "../Modal/DonatRequest";
+
+
 
 
 const Banner2 = () => {
+  const { user } = useContext(AuthContext);
+  console.log(user);
   return (
     <>
       <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
@@ -23,17 +30,19 @@ const Banner2 = () => {
           >
             <div className="max-w-screen-xl mx-auto min-h-[calc(100vh-90px)] flex items-center">
               <div className="text-white space-y-5 lg:px-0 md:px-10 px-5">
-                <h3 className="lg:text-2xl md:text-xl text-lg">
+                <h3 className="lg:text-3xl md:text-xl text-lg text-red-500 font-bold">
                   Donate blood,save life!
                 </h3>
                 <h2 className="lg:text-6xl md:text-5xl text-3xl font-bold lg:leading-[70px] md:leading-[60px]">
-                  YOUR BLOOD <br /> CAN BRING SMILE <br /> IN OTHER PERSON FACE
+                  YOUR <span className="text-red-600 ">BLOOD</span> <br /> CAN BRING SMILE <br /> IN OTHER PERSON FACE
                 </h2>
                 <div className="flex gap-5 md:flex-row flex-col">
-                  {/* <button  className="btn bg-primary hover:bg-secondary border-0 text-white rounded-none px-10 md:text-lg text-base  font-medium">
-                    Donate Now
-                  </button> */}
-
+                  {
+                    user && <><button onClick={() => document.getElementById("my_modal_5").showModal()} className="btn bg-primary hover:bg-secondary border-0 text-white rounded-none px-10 md:text-lg text-base  font-medium">
+                      Donate Now
+                    </button></>
+                  }
+                  <DonatRequest></DonatRequest>
                   <button className="btn bg-gray-700 hover:bg-primary border-0 text-white rounded-none px-10 md:text-lg text-base font-medium">
                     Call: +91 234 5678
                   </button>
